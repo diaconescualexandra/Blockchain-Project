8dfb038fae83ecf45f4e8f885fd7f6fcceb2a04f
Alexandra Diaconescu <alexandraisi46@gmail.com>
2025-01-11 19:00:00 +0200

    update for deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
 require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-chai-matchers");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
     hardhat: {
       chainId: 31337
     }
   }
+}
 };
